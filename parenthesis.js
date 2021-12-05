0af7fdf57b5247c383279367307f91e850212f90
OMAR-FARUK-SHAKIL <omarshakil8@gmail.com>
2021-12-05 21:36:49 +0600

    javaScript coding

---
+++ parenthesis.js
@@ -0,0 +1,10 @@
+//find out the odd parenthesis that stays in your string which you take as an input
+//suppose [()]({(])} is input
+//then 2 is your output
+
+let brackets = "[()]({(])}"
+let len = brackets.length;
+let brac = [];
+let a = brackets.indexOf('[');
+console.log(len)
+console.log(a)
