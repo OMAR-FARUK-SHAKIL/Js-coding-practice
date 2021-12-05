262eeaf72d68d602efc3a17988a8afa62b54be7d
OMAR-FARUK-SHAKIL <omarshakil8@gmail.com>
2021-12-05 23:54:33 +0600

    code added

--- parenthesis.js
+++ parenthesis.js
@@ -2,9 +2,25 @@
 //suppose [()]({(])} is input
 //then 2 is your output
 
-let brackets = "[()]({(])}"
+let brackets = "[(}{){]"
 let len = brackets.length;
 let brac = [];
-let a = brackets.indexOf('[');
-console.log(len)
+let a = brackets.split('');
+let overflow = 0 ;
+
+for(i=0;i<a.length;i++)
+{
+    if(a[i] == "(" || a[i] == "{"  || a[i] == "["){
+        brac.push(a[i]);
+    }
+    else if(a[i] == ")" || a[i] == "}"  || a[i] == "]"){
+       brac.length == 0 ? overflow++ : brac.pop();
+    }
+
+}
+let ans = (overflow == 0)? "Balanced" : `Non-Balanced,overflow ${overflow}`
+
+
+console.log(brac.length)
 console.log(a)
+console.log(ans)
